47526d687417a62dfa7f6a1c14474da370d34894
SanKan <2475986535@qq.com>
2026-04-23 17:28:04 +0800

    feat: 添加底部导航栏图标和样式文件

--- sihua-miniprogram/src/app.config.ts
+++ sihua-miniprogram/src/app.config.ts
@@ -19,15 +19,21 @@ export default defineAppConfig({
     list: [
       {
         pagePath: 'pages/index/index',
-        text: '首页'
+        text: '首页',
+        iconPath: 'assets/tabbar/home.png',
+        selectedIconPath: 'assets/tabbar/home-active.png'
       },
       {
         pagePath: 'pages/history/index',
-        text: '历史'
+        text: '历史',
+        iconPath: 'assets/tabbar/history.png',
+        selectedIconPath: 'assets/tabbar/history-active.png'
       },
       {
         pagePath: 'pages/mine/index',
-        text: '我的'
+        text: '我的',
+        iconPath: 'assets/tabbar/mine.png',
+        selectedIconPath: 'assets/tabbar/mine-active.png'
       }
     ]
   }
